060ae58c49ef381a786de126261fb8aa861fd8a2
rosericazondekon <poj2@cdc.gov>
2021-11-06 05:09:02 -0400

    Add CI/CD gh-pages pipeline

--- R/build2.R
+++ R/build2.R
@@ -1,4 +1,4 @@
 # An optional custom script to run after Hugo builds your site.
-# from <- file.path(getwd(), "public")
-# to <- file.path(dirname(getwd()), "Rnssp-rmd-templates", "docs")
-# fs::dir_copy(from, to, overwrite = TRUE)
+from <- file.path(getwd(), "public")
+to <- file.path(dirname(getwd()), "Rnssp-rmd-templates", "docs")
+fs::dir_copy(from, to, overwrite = TRUE)
